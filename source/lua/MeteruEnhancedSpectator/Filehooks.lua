f3fddb180d7f86a063d2833d4fafbd7bb8e2deb1
ailmanki <1570625+ailmanki@users.noreply.github.com>
2017-09-16 23:21:10 +0200

    checking in new function to change

--- source/lua/MeteruEnhancedSpectator/Filehooks.lua
+++ source/lua/MeteruEnhancedSpectator/Filehooks.lua
@@ -1,2 +1,3 @@
+ModLoader.SetupFileHook( "lua/FirstPersonSpectatorMode.lua", "lua/MeteruEnhancedSpectator/FirstPersonSpectatorMode.lua", "post" )
 ModLoader.SetupFileHook( "lua/Player_Server.lua", "lua/MeteruEnhancedSpectator/Player_Server.lua", "post" )
 ModLoader.SetupFileHook( "lua/Spectator.lua", "lua/MeteruEnhancedSpectator/Spectator.lua", "post" )
---
+++ source/lua/MeteruEnhancedSpectator/FirstPersonSpectatorMode.lua
@@ -0,0 +1,56 @@
+function FirstPersonSpectatorMode:CycleSpectatingPlayer(spectatingEntity, spectatorEntity, client, forward)
+
+    -- Find a valid target to follow.
+    local targets = spectatorEntity:GetTargetsToFollow()
+    -- Remove any non-players from the list.
+    for t = #targets, 1, -1 do
+
+        local target = targets[t]
+        if not target:isa("Player") then
+            table.remove(targets, t)
+        end
+
+    end
+
+    local numTargets = #targets
+    local validTargetIndex = numTargets > 0 and math.random(1, numTargets) or nil
+    -- Look for the current spectatingEntity index.
+    for t = 1, #targets do
+
+        if targets[t] == spectatingEntity then
+
+            validTargetIndex = t
+            break
+
+        end
+
+    end
+
+    -- Fall back on Following mode if there is no other target.
+    if numTargets == 0 then
+
+        spectatorEntity:SetSpectatorMode(kSpectatorMode.Following)
+        return true
+
+    elseif validTargetIndex then
+
+        -- Find the next index and cycle around if needed.
+        if forward then
+            validTargetIndex = validTargetIndex < #targets and validTargetIndex + 1 or 1
+        else
+            validTargetIndex = validTargetIndex > 1 and validTargetIndex - 1 or #targets
+        end
+
+        local finalTargetEnt = targets[validTargetIndex]
+        if spectatingEntity ~= finalTargetEnt then
+
+            client:SetSpectatingPlayer(finalTargetEnt)
+            return true
+
+        end
+
+    end
+
+    return false
+
+end
